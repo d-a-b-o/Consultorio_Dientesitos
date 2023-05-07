e5480acaccd0edb3ae7ec579964f5fdf073e29c1
D-A-B-O <bravodavid451@gmail.com>
2023-05-07 05:33:27 -0500

    feat: Add CSS styles to project

--- WebDientesitos/WebDientesitos/Controllers/HomeController.cs
+++ WebDientesitos/WebDientesitos/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ namespace WebDientesitos.Controllers
             return View();
         }
 
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
+        public IActionResult Servicios()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
